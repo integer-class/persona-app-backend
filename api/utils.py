6e980a943f0588ef98261db1dacd95c68b5afc0a
srikresna <srikresna383@gmail.com>
2024-11-21 00:47:23 +0700

    feat: add user profile signals and media URL handling

---
+++ api/utils.py
@@ -0,0 +1,15 @@
+import os
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class PathAndRename:
+    def __init__(self, sub_path):
+        self.sub_path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(self.sub_path, filename)
