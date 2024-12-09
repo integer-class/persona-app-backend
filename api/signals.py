68b69e09dd32a9cdaa40a4ce985d49acf31000b9
srikresna <srikresna383@gmail.com>
2024-12-10 00:01:26 +0700

    refactor: remove unused history cache logic

--- api/signals.py
+++ api/signals.py
@@ -1,8 +1,7 @@
-from django.db.models.signals import post_save, post_delete
-from django.core.cache import cache
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile, History
+from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -12,8 +11,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
-@receiver(post_save, sender=History)
-@receiver(post_delete, sender=History)
-def clear_history_cache(sender, instance, **kwargs):
-    cache.delete_pattern('views.decorators.cache.cache_page.*')
